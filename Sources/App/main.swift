2df7c960bb997a4b4d080a6f421f5fce301241e1
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2019-04-09 15:41:59 +0200

    2

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -16,46 +16,30 @@ do {
         descriptive = (readLine()?.lowercased().first?.description ?? "n") == "y"
     }
     
-    /*
-    var grid = Grid(size: 5)
-    
-    grid.visibilities.top = [0, 4, 1, 0, 0]
-    grid.visibilities.left = [0, 0, 0, 0, 4]
-    grid.visibilities.right = [0, 0, 0, 0, 1]
-    grid.visibilities.bottom = [4, 0, 3, 0, 0]
-    */
-    
     var grid = Grid(size: 6)
-    
+
+    /* // 1
     grid.visibilities.top = [2, 2, 3, 2, 4, 1]
     grid.visibilities.left = [4, 1, 2, 3, 2, 2]
     grid.visibilities.right = [1, 4, 3, 2, 2, 2]
     grid.visibilities.bottom = [3, 4, 1, 2, 2, 2]
-    
-    // Pre-solve a few cells (needed for the solution)
     grid.markSolved(x: 4, y: 0, height: 1)
     grid.markSolved(x: 3, y: 1, height: 1)
     grid.markSolved(x: 0, y: 2, height: 2)
     grid.markSolved(x: 2, y: 3, height: 3)
     grid.markSolved(x: 3, y: 4, height: 6)
-    
-    // var grid = Grid(size: 8)
-    
-    // grid.visibilities.top = [0, 0, 3, 1, 5, 0, 0, 4]
-    // grid.visibilities.left = [3, 3, 2, 1, 0, 4, 0, 3]
-    // grid.visibilities.right = [0, 0, 0, 3, 3, 0, 4, 0]
-    // grid.visibilities.bottom = [0, 0, 1, 4, 0, 2, 0, 0]
-    
-    // grid.markSolved(x: 5, y: 0, height: 3)
-    // grid.markSolved(x: 0, y: 2, height: 4)
-    // grid.markSolved(x: 6, y: 2, height: 3)
-    // grid.markSolved(x: 2, y: 3, height: 4)
-    // grid.markSolved(x: 1, y: 4, height: 1)
-    // grid.markSolved(x: 7, y: 4, height: 4)
-    // grid.markSolved(x: 2, y: 5, height: 6)
-    // grid.markSolved(x: 7, y: 6, height: 2)
-    // grid.markSolved(x: 1, y: 7, height: 5)
-    // grid.markSolved(x: 4, y: 7, height: 1)
+    */
+
+    // 2
+    grid.visibilities.top = [2, 1, 3, 3, 2, 4]
+    grid.visibilities.left = [2, 1, 3, 3, 3, 2]
+    grid.visibilities.right = [3, 3, 2, 1, 3, 2]
+    grid.visibilities.bottom = [2, 3, 2, 2, 1, 2]
+    grid.markSolved(x: 0, y: 0, height: 2)
+    grid.markSolved(x: 3, y: 1, height: 3)
+    grid.markSolved(x: 4, y: 3, height: 1)
+    grid.markSolved(x: 3, y: 4, height: 6)
+    grid.markSolved(x: 5, y: 4, height: 1)
     
     let solver = Solver(grid: grid)
     solver.interactive = interactive
