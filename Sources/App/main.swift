ca0f7006df029b784ff877f0b59a44de1772c4d6
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2019-04-09 15:53:21 +0200

    5

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -54,13 +54,26 @@ do {
     grid.markSolved(x: 0, y: 4, height: 4)
     */
 
-    // 4
+    /* // 4
     grid.visibilities.top = [3, 2, 4, 1, 2, 2]
     grid.visibilities.left = [3, 2, 6, 1, 2, 3]
     grid.visibilities.right = [2, 2, 1, 3, 4, 2]
     grid.visibilities.bottom = [3, 2, 1, 4, 4, 2]
     grid.markSolved(x: 0, y: 0, height: 4)
     grid.markSolved(x: 4, y: 0, height: 1)
+    */
+
+    // 5
+    grid.visibilities.top = [3, 2, 2, 1, 3, 4]
+    grid.visibilities.left = [3, 2, 1, 2, 3, 2]
+    grid.visibilities.right = [3, 3, 3, 2, 2, 1]
+    grid.visibilities.bottom = [2, 3, 3, 4, 2, 1]
+    grid.markSolved(x: 3, y: 1, height: 5)
+    grid.markSolved(x: 1, y: 2, height: 4)
+    grid.markSolved(x: 2, y: 2, height: 3)
+    grid.markSolved(x: 2, y: 3, height: 6)
+    grid.markSolved(x: 2, y: 4, height: 5)
+    grid.markSolved(x: 5, y: 4, height: 3)
     
     let solver = Solver(grid: grid)
     solver.interactive = interactive
